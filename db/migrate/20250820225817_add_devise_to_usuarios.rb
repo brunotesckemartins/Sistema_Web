1280b166e4cb146c8aa73d6fbd9fa6b02f2114b4
brunotescke <brunotesckemartins@gmail.com>
2025-08-23 16:20:03 -0300

    ajuste em arquivos para push

--- db/migrate/20250820225817_add_devise_to_usuarios.rb
+++ db/migrate/20250820225817_add_devise_to_usuarios.rb
@@ -4,7 +4,7 @@ class AddDeviseToUsuarios < ActiveRecord::Migration[8.0]
   def self.up
     change_table :usuarios do |t|
       ## Database authenticatable
-      #t.string :email,              null: false, default: ""
+      # t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
