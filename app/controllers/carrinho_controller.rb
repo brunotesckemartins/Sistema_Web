4801b163e72c844bce9b8eff3a4055b54ec6f37b
brunotescke <brunotesckemartins@gmail.com>
2025-09-25 23:08:54 -0300

    ajustes carrinho

--- app/controllers/carrinho_controller.rb
+++ app/controllers/carrinho_controller.rb
@@ -2,7 +2,7 @@ class CarrinhoController < ApplicationController
   def add_to_carrinho
     session[:carrinho] ||= {}
 
-    produto_id = params[:produto_id].to_s
+    produto_id = params[:produto_id]
 
     session[:carrinho][produto_id] = (session[:carrinho][produto_id].to_i + 1)
 
@@ -12,14 +12,14 @@ class CarrinhoController < ApplicationController
   def show
     ids_produtos = (session[:carrinho] || {}).keys.map(&:to_i)
 
-    @produtos_do_carrinho = Produto.where(id: ids_produtos)
+    @produtos_do_carrinho = Produto.where(id: ids_produtos).to_a
+
   rescue ActiveRecord::RecordNotFound
     @produtos_do_carrinho = []
   end
 
   def remover_do_carrinho
-    produto_id = params[:produto_id].to_s
-    session[:carrinho]&.delete(produto_id)
-    redirect_to carrinho_path, notice: "Produto removido do carrinho."
+    session[:carrinho].delete(params[:produto_id])
+    redirect_to produtos_path, notice: "Produto removido do carrinho!"
   end
 end
