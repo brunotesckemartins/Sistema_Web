604ab0818883cc418eaff03949d74bded8c439d3
brunotesckemartins <brunotesckemartins@gmail.com>
2025-12-01 15:54:29 -0300

    .

--- app/controllers/autenticacao/sessions_controller.rb
+++ app/controllers/autenticacao/sessions_controller.rb
@@ -1,25 +1,38 @@
 class Autenticacao::SessionsController < Devise::SessionsController
-  respond_to :html, only: [ :new ]
-  respond_to :json, only: [ :create, :destroy ]
 
   def create
     self.resource = warden.authenticate(auth_options)
-
+    
     if resource
       sign_in(resource_name, resource)
-      render json: {
-        message: "Login com sucesso.",
-        data: resource
-      }, status: :ok
+      
+      respond_to do |format|
+        format.json { 
+          render json: {
+            message: 'Login com sucesso.',
+            data: resource
+          }, status: :ok 
+        }
+      end
     else
-      render json: {
-        message: "Email ou senha inválidos."
-      }, status: :unauthorized
+      respond_to do |format|
+        format.json {
+          render json: {
+            message: 'Email ou senha inválidos.'
+          }, status: :unauthorized
+        }
+      end
     end
   end
 
+  # DELETE /auth/sign_out
   def destroy
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    render json: { message: "Logout com sucesso." }, status: :ok
+    
+    respond_to do |format|
+      format.json { render json: { message: 'Logout com sucesso.' }, status: :ok }
+      
+      format.html { redirect_to root_path, notice: 'Você saiu com sucesso.' }
+    end
   end
 end
